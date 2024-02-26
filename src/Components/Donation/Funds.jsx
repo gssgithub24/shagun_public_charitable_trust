b9923298e8985505bf339cb292eeaaaf8cc23ca6
Ramkrushna12 <ramkrushnasahu278@gmail.com>
2024-02-26 15:19:21 +0530

    Donation Page completed

---
+++ src/Components/Donation/Funds.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import DonationPage from '../../public/DonationPage.png'
+import Progress from './Progress'
+
+function Funds({ img, title, description, createdBy, amount, progress, target }) {
+  return (
+    <>
+      <div className='mt-6 font-sans mb-3'>
+        <div className='container mx-auto xl:flex xl:justify-center block'>
+          <div className='xl:w-[33%] w-[90%] xl:mx-0 mx-auto'>
+            <img src={img} alt="" className='w-[100%] rounded-xl' />
+          </div>
+          <div className='xl:px-10 md:px-11 px-6 xl:w-[50%] w-full xl:mt-0 mt-3'>
+            <p className='font-semibold text-4xl'>{title}</p>
+            <p className='text-sm font-light mt-1'>{description}</p>
+            {/* <p className='text-sm'><span className='text-orange-500 font-medium'>Campaign created by</span>&ensp;&emsp;&nbsp;:  Campaigncreator1</p>
+            <p className='text-sm'><span className='text-orange-500 font-medium'>Raised amount in rupees</span>&nbsp;:  ₹7, 09, 371</p>
+            <p className='text-sm'><span className='text-orange-500 font-medium'>Campaign progress</span>&ensp;&emsp;&emsp;&nbsp;:  Campaigncreator1</p> */}
+            <p className='md:text-sm text-xs mt-1'><span className='text-orange-400 font-semibold mr-[26px]'>Campaign created by</span>:  {createdBy}</p>
+            <p className='md:text-sm text-xs'><span className='text-orange-400 font-semibold md:mr-[4px] mr-[6px]'>Raised amount in rupees</span>:  {amount}</p>
+            <div className='flex md:items-center md:flex-row flex-col items-start'>
+              <div className="flex items-center w-full md:w-[85%]">
+                <p className='md:text-sm text-xs'><span className='text-orange-400 font-semibold md:mr-[37px] mr-[35px]'>Campaign progress</span>:</p>  
+                <Progress progress={progress}/>
+              </div>
+              <p className="md:text-sm text-xs ml-0 font-medium mt-1 md:mt-0">Target : {target}</p>
+            </div>
+
+
+            <button className='bg-orange-400 hover:bg-orange-600 px-12 py-3 mt-5 text-white rounded-md'>Donate</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Funds
---
+++ src/Components/Donation/TopDonaters.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Image from '../../public/Image.png'
+const TopDonators = () => {
+    const imgSrc = "https://static.vecteezy.com/system/resources/previews/000/546/107/original/businessman-in-suit-head-vector-icon.jpg";
+    return (
+        <>
+            <div className="px-4 pt-6 pb-10 md:px-14 md:pt-8 md:pb-14">
+                <div className="md:mb-6 mb-4">
+                    <p className="font-normal md:text-3xl text-xl md:mb-2 mb-1">Let others smile big with your small contributions</p>
+                    <p className="font-normal md:text-xl text-sm">Meet our top donaters</p>
+                </div>
+                <div className="relative flex justify-end items-start left-0">
+                    {[1, 2, 3, 4, 5, 6, 7].map((index) => (
+                        <div
+                            key={index}
+                            style={{
+                                // left: `${index * 30}px`,
+                                left: `${index === 1 ? 0 : index * 30}px`, // Reduce left space for the first image
+                                marginLeft: index === 1 ? '3px' : '-27px',
+                                backgroundImage: `url('${imgSrc}')`,
+                                backgroundSize: 'cover'
+                            }}
+                            className="absolute w-16 h-16 rounded-full border border-black z-10 mr-4 shadow-lg"
+                        ></div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default TopDonators;
